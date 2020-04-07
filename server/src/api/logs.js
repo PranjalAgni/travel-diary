8e750685b070f1b6c16f5bcfbe78d3f4a586f289
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-04-07 22:14:47 +0530

    :sparkles: added rate-limiter with mongo-store to limit resource creation

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -1,8 +1,20 @@
 const { Router } = require('express');
+const rateLimit = require('express-rate-limit');
+const MongoStore = require('rate-limit-mongo');
 
 const LogEntry = require('../models/LogEntry');
 
 const router = Router();
+
+const rateTimeDelay = 20 * 1000; // 20 sec
+const createLogsLimiter = rateLimit({
+  store: new MongoStore({
+    uri: process.env.MONGODB_URL,
+    expireTimeMs: rateTimeDelay
+  }),
+  windowMs: rateTimeDelay,
+  max: 100
+});
 
 router.get('/', async (req, res, next) => {
   try {
@@ -14,11 +26,28 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', createLogsLimiter, async (req, res, next) => {
   try {
     const logEntry = new LogEntry(req.body);
     const createdEntry = await logEntry.save();
     res.json(createdEntry);
+  } catch (error) {
+    if (error.name === 'ValidationError') res.status(422);
+    next(error);
+  }
+});
+
+router.get('/date', async (req, res, next) => {
+  try {
+    const endDate = new Date();
+    const startDate = new Date(2020, 01, 15);
+    const logEntryByDate = await LogEntry.find({
+      visitDate: {
+        $gte: startDate,
+        $lte: endDate
+      }
+    }).lean();
+    res.json(logEntryByDate);
   } catch (error) {
     if (error.name === 'ValidationError') res.status(422);
     next(error);
