28a5ff28eb64b60c130e0ce43d1a23c9ee7a54a2
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-04-07 22:15:42 +0530

    added db config + auto-reconnect if db is disconnected

---
+++ server/src/utils/db.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
+
+function connectDB() {
+  mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
+}
+
+mongoose.connection.on('error', function (err) {
+  console.log('Error connecting to DB: ', err);
+});
+
+mongoose.connection.on('disconnected', function () {
+  console.log('Lost MongoDB connection.');
+  console.log('Retrying...');
+  connectDB();
+});
+
+mongoose.connection.on('connected', function () {
+  console.log('Connected to DB');
+});
+
+mongoose.connection.on('reconnected', function () {
+  console.log('Reconnected...');
+});
+
+// SIGINT signal works in linux
+process.on('SIGINT', function () {
+  mongoose.connection.close(function () {
+    console.log('Force to close the mongodb connection');
+    process.exit(0);
+  });
+});
+
+module.exports = {
+  connectDB
+};
