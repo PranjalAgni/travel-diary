0ac8f10b135085d7f111b60fc93347996312dc2d
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-04-21 00:38:43 +0530

    :zap: hash password before saving + method for verifying password hash

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator').default;
+const bcrypt = require('bcryptjs');
 
 const { Schema } = require('mongoose');
 
@@ -32,6 +33,7 @@ const usersSchema = new Schema(
     email: {
       ...requiredTrimString,
       lowercase: true,
+      unique: true,
       validate(value) {
         if (!validator.isEmail(value)) {
           throw new Error('Invalid email provided');
@@ -39,12 +41,7 @@ const usersSchema = new Schema(
       }
     },
     birthDate: {
-      type: Date,
-      validate(value) {
-        if (!validator.isISO8601(value)) {
-          throw new Error('Invalid date provided');
-        }
-      }
+      type: Date
     },
     spots: [
       {
@@ -57,6 +54,33 @@ const usersSchema = new Schema(
     timestamps: true
   }
 );
+
+// helper method when user is trying to login
+usersSchema.statics.findByCredentials = async (email, password) => {
+  const user = await Users.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
+
+// Hash the plain text password before saving
+usersSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
 
 const Users = mongoose.model('Users', usersSchema);
 
