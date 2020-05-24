4814edf81131a23aa3de08062df6ce9276094cb4
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-05-24 23:30:59 +0530

    :sunflower: list users + code refactoring

--- server/src/api/users.js
+++ server/src/api/users.js
@@ -1,8 +1,19 @@
 const { Router } = require('express');
 
 const Users = require('../models/Users');
+const { formatResponse } = require('../utils/responseFormatter');
+const { omitKey } = require('../utils/lodash');
 
 const router = Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await Users.find().lean();
+    formatResponse(res, omitKey(users, ['password', 'createdAt', 'updatedAt']));
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post('/register', async (req, res, next) => {
   try {
@@ -10,7 +21,7 @@ router.post('/register', async (req, res, next) => {
     const createdUser = await user.save();
     const sanatizedResponse = createdUser.toObject();
     delete sanatizedResponse.password;
-    res.json(sanatizedResponse);
+    formatResponse(res, sanatizedResponse);
   } catch (error) {
     if (error.name === 'ValidationError') res.status(422);
     next(error);
@@ -23,10 +34,9 @@ router.post('/login', async (req, res, next) => {
       req.body.email,
       req.body.password
     );
-    res.json({
-      status: 'Success',
-      message: 'Login'
-    });
+
+    const token = await user.generateAuthToken();
+    formatResponse(res, { token });
   } catch (error) {
     res.status(400);
     next(error);
